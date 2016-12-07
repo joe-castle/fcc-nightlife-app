7a37aa787e0977d21cfb433c504365e220dc2788
Joe Smith <joesmith0488@gmail.com>
2016-12-07 13:44:37 +0000

    front-end prototype done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,69 @@
 import React from 'react';
 
+function Bars({ bars, handleGoingClick }) {
+  return (
+    <section>
+      {bars.map(bar => (
+        <section key="bar.id">
+          <img src={bar.img_url} alt={bar.name}/>
+          <h4>{bar.name}</h4>
+          <p>{bar.description}</p>
+          <button onClick={() => handleGoingClick(bar.id)}>{`${bars.going.length} Going`}</button>
+        </section>
+      ))}
+    </section>
+  );
+}
+
 // Class used as hot-reloader does not work with pure function components
 // as the entry point.
 export class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      bars: [],
+    };
+  }
+
+  handleSubmit = ev => {
+    ev.preventDefault();
+
+    fetch(`/api/bars?area=${ev.target.value}`)
+      .then(res => res.json())
+      .then(json => console.log('json', json))
+      .catch(error => console.log('error', error))
+  }
+
+  handleGoingClick = id => {
+
+  }
+
   render() {
     return (
       <div className="App">
-        <main>
-          <h1>Nightlife Apps</h1>
+        <main style={{ 
+          margin: '0 auto',
+          maxWidth: '800px', 
+        }}>
+          <h1 style={{
+            fontSize: '3em',
+            textAlign: 'center',
+          }}>
+            Whats going on in your local bars?
+          </h1>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              ref={c => { this.cityInput = c; }}
+              placeholder="Please enter your area..."
+              style={{
+                display: 'block',
+                margin: '0 auto',
+                width: '30em',
+              }}
+            />
+          </form>
+          <Bars bars={this.state.bars} handleGoingClick={this.handleGoingClick} />
         </main>
       </div>
     );
